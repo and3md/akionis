c9ceb493de4b05b017c9304dad23b328e9832035
and3md <and3md@gmail.com>
2026-04-20 19:02:11 +0200

    Added yoellow color const

--- src/akionis/colors.nim
+++ src/akionis/colors.nim
@@ -6,5 +6,5 @@ const Red* = Color(r: 255, g: 0, b: 0, a: 255)
 const Blue* = Color(r: 0, g: 0, b: 255, a: 255)
 const White* = Color(r: 255, g: 255, b: 255, a: 255)
 const Black* = Color(r: 0, g: 0, b: 0, a: 255)
-
+const Yellow* = Color(r: 255, g: 255, b: 0, a: 255)
 
