748f35cc214f2868a2bdb62604a894b7932ec35f
and3md <and3md@gmail.com>
2026-04-27 16:15:47 +0200

    A lot of fixes for VLayout Component

--- src/akionis/components/ui/vlayout.nim
+++ src/akionis/components/ui/vlayout.nim
@@ -8,40 +8,48 @@ from raylib as ray import nil
 type VLayout = ref object of UiComponent
   usedHeight: int32
 
+proc newVLayout*(name: string): VLayout =
+  result = new(VLayout)
+  initUiComponent(result, name)
+
 method draw*(comp: VLayout, camera: Camera) =
   discard
 
-method updateSize*(comp: VLayout, availableArea: Rect) =
+method updateSize*(comp: VLayout, availableSize: Size) =
   ## Method to update size with children, we runt this only on root ui node
   ## Children are calculated recursively
-  var newSize = comp.calculatedMinSize
+
+  var newSize = availableSize
   applyMinMaxSize(newSize, comp.minSize, comp.maxSize)
   let parent = comp.parent
 
   # Phase 1: Update children size
   if parent.isNil:
-    # no parent Node o just return 
+    # no parent Node so just return
     return
 
   var children: seq[tuple[node: Node, comp: UiComponent]]
   for r in parent.getChildrenWithUi:
     if r.comp.isExisting:
       children.add(r)
-      comp.updateSize(availableArea)
+      r.comp.updateSize(availableSize)
 
+  # Phase 2: Set position and calculate used space without 
   var usedSpace: int32 = 0
   var heightFactorSum: int32 = 0
-  var x: int32 = 0
-  # Phase 2: Set position and calculate used space without 
+  var y: int32 = 0
+  
+
   for r in children:
-    r.node.x = float32(x + usedSpace)
-    r.node.y = 0'f32 # temporary simplification
+    r.node.x = 0'f32 # temporary simplification
+    r.node.y = float32(y + usedSpace)
     usedSpace += r.comp.calculatedMinSize.height
+    heightFactorSum += r.comp.heightFactor
 
   # Phase 3: Expand children to use remaining space - TODO
 
   newSize.height = usedSpace
-
+  echo "vlayout newSize ", newSize
   if comp.size == newSize:
     return
   comp.size = newSize
