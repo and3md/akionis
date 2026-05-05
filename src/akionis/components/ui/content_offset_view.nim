9c78b8d5b5868c2b1134e9518d7ed693e8cf0cea
and3md <and3md@gmail.com>
2026-05-05 15:26:20 +0200

    First version of ContentOffsetView.updateLayout()

--- src/akionis/components/ui/content_offset_view.nim
+++ src/akionis/components/ui/content_offset_view.nim
@@ -29,3 +29,34 @@ method calculateMinSize*(comp: ContentOffsetView) =
   else:
     comp.contentMinSize = Size(width: 0, height: 0)
 
+method updateLayout*(comp: ContentOffsetView, availableSize: Size) =
+  var newSize = availableSize
+  applyMinMaxConstraint(newSize, comp.minConstraint, comp.maxConstraint)
+
+  let parent = comp.parent
+
+  if parent.isNil:
+    # no parent Node so just return
+    return
+
+  let child = comp.parent.getFirstChildWithUiComponent()
+  if child.isSome:
+    let (childNode, childComp) = child.get()
+    childNode.x = float32(comp.padding.left + comp.contentOffsetX)
+    childNode.y = float32(comp.padding.top + comp.contentOffsetY)
+
+    # when newSize is bigger than contentMinSize, check child factor's and if they 
+    # are greater than zero increase contentSize otherwise its simply contentMinSize
+
+    var childSize = comp.contentMinSize
+    # width
+    if newSize.width - comp.padding.left - comp.padding.right > comp.contentMinSize.width and
+        childComp.widthFactor > 0:
+      childSize.width = newSize.width - comp.padding.left - comp.padding.right
+    if newSize.height - comp.padding.top - comp.padding.bottom >
+        comp.contentMinSize.height and childComp.heightFactor > 0:
+      childSize.height = newSize.height - comp.padding.top - comp.padding.bottom
+    childComp.size = childSize
+    comp.contentSize = childSize
+    childComp.updateLayout(childComp.size)
+  comp.size = newSize
