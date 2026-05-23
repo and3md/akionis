6bb742f2196251e17def6417ce7a7bb30dd6877b
and3md <and3md@gmail.com>
2026-05-23 14:42:58 +0200

    Started work on the input singleton

---
+++ src/akionis/input.nim
@@ -0,0 +1,51 @@
+import events
+from raylib as ray import nil
+import matrices
+import std/sequtils
+
+type 
+  InputAction = ref object of RootObj
+    name: string
+    ## Future input action to be implemented
+
+  Input* = ref object of RootObj
+    ## Gives access to input state with and without ui filtering
+    leftClickHandledByUi: bool
+    mouseMoveHandledByUi: bool
+    pressedKeyshandledByUi: seq[KeyboardKey]
+    actions: seq[InputAction]
+
+var instance: Input = nil
+
+proc getInput*(): Input =
+  if instance.isNil:
+    instance = new(Input)
+  return instance
+
+proc reset*(input: Input) =
+  ## Resets input state used on frame start
+  input.leftClickHandledByUi = false
+  input.mouseMoveHandledByUi = false
+  input.pressedKeyshandledByUi.setLen(0)
+
+
+proc getMouseMoveDeltaUnfiltered*(input: Input): Vector2 =
+  ## Gets mouse move delta from previous frame without checking was it handled by ui
+  return ray.getMouseDelta()
+
+proc isMouseButtonPressedUnfiltered*(input: Input, button: MouseButton): bool =
+  ## Was mouse button presed in this frame (without checking was it handled by ui)
+  return ray.isMouseButtonReleased(button)
+
+proc isMouseButtonDownUnfiltered*(input: Input, button: MouseButton): bool =
+  ## Is mouse button still down (without checking was it handled by ui)? 
+  return ray.isMouseButtonDown(button)
+
+proc isMouseButtonUpUnfiltered*(input: Input, button: MouseButton): bool =
+  ## Is mouse button still up (without checking was it handled by ui)? 
+  return ray.isMouseButtonUp(button)
+
+proc isMouseButtonReleasedUnfiltered*(input: Input, button: MouseButton): bool =
+  ## Was mouse button released in this frame (without checking was it handled by ui)
+  return ray.isMouseButtonReleased(button)
+
