efbee67d0442596beb932b667140dd887357172a
and3md <and3md@gmail.com>
2026-09-21 19:39:17 +0200

    Blank window created with raylib.

--- src/app/akionis-app.adb
+++ src/app/akionis-app.adb
@@ -1,12 +1,33 @@
 with Ada.Wide_Text_IO;
-
 use Ada;
+with Raylib;
+
+with Ada.Strings.UTF_Encoding.Wide_Wide_Strings;
+use Ada.Strings.UTF_Encoding.Wide_Wide_Strings;
+
+with Interfaces.C; use Interfaces.C;
 
 package body Akionis.App is
 
    procedure Init is
    begin
       Ada.Wide_Text_IO.Put_Line ("Test łóąźż");
+
+      Raylib.InitWindow
+        (width => 1670, height => 800, title => Encode ("Test łóążź 🚀"));
+      Raylib.SetExitKey (key => Raylib.KEY_ESCAPE);
+
+      while Raylib.WindowShouldClose = False loop
+         Raylib.BeginDrawing;
+
+         if Raylib.IsKeyDown (Raylib.KEY_ESCAPE) then
+            Wide_Text_IO.Put_Line ("Close");
+         end if;
+         Raylib.EndDrawing;
+      end loop;
+
+      Raylib.CloseWindow;
+
    end Init;
 
 end Akionis.App;
--- src/app/akionis-app.ads
+++ src/app/akionis-app.ads
@@ -2,4 +2,10 @@ package Akionis.App is
 
    procedure Init;
 
+private
+
+   pragma Linker_Options ("-lraylib");
+   pragma Linker_Options ("-lm");
+   pragma Linker_Options ("-lX11");
+
 end Akionis.App;
